bb09b720608c5481727a92ed9114a2122fdba18c
Amzadbasha Shaik <amzads@ispace.com>
2021-01-04 13:44:53 +0530

    Initial commit

---
+++ src/main/java/com/spring/boot/practice/SpringBootCurrencyConversionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurrencyConversionApplication.class, args);
+	}
+
+}
